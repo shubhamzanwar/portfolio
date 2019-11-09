975c3b13fabcf6a55684127c24807522ecc589e0
ShubhamZanwar <zanwar.shubham@gmail.com>
2019-11-09 21:41:01 +0530

    icons: setting up font awesome

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { config, library } from '@fortawesome/fontawesome-svg-core'
+import { faCode, faTools, faBriefcase } from '@fortawesome/free-solid-svg-icons'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
+config.autoAddCss = false;
+library.add(faCode, faTools, faBriefcase);
 
 import DefaultLayout from '~/layouts/Default.vue'
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  Vue.component('font-awesome', FontAwesomeIcon)
 }
