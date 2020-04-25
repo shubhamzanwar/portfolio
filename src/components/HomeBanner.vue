f00ccbc602f776c42152bf3757ceb95d882fb68b
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-04-25 14:52:30 +0530

    adding the page banner component

---
+++ src/components/HomeBanner.vue
@@ -0,0 +1,24 @@
+<template>
+    <banner
+        title="Hi, I'm Shubham Zanwar"
+        subTitle="A full stack software development engineer"
+    >
+        <template v-slot:image>
+            <g-image src="~/assets/home.png" />
+        </template>
+    </banner>
+</template>
+
+<script>
+import Banner from "./Banner";
+
+export default {
+  components: {
+    banner: Banner
+  }
+}
+</script>
+
+<style>
+
+</style>
