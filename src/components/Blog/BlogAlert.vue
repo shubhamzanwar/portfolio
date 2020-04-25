42dddc313887d809ade996c9fa268ac014c797d0
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-04-25 20:31:21 +0530

    adding bloig image and banner typo

--- src/components/Blog/BlogAlert.vue
+++ src/components/Blog/BlogAlert.vue
@@ -2,8 +2,8 @@
     <div class="spacer">
         <alert type="info">
             All of these articles are available on 
-            <a href="https://github.com/shubhamzanwar" target="_blank">medium</a>. I also soon play to start writing on
-            <a href="https://dev.to/shubhamzanwar">Dev.to</a>.
+            <a href="https://github.com/shubhamzanwar" target="_blank">medium</a>. I also soon plan to start writing on
+            <a href="https://dev.to/shubhamzanwar" target="_blank">Dev.to</a>.
         </alert>
     </div>
 </template>
