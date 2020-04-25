fa84cd2b19a99d850e3b877b48b48d3610ccb707
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-04-25 19:04:02 +0530

    adding projects page alert

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -2,12 +2,14 @@
     <Layout>
         <div class="projects-page">
             <projects-banner />
+            <projects-alert />
         </div>
     </Layout>
 </template>
 
 <script>
 import ProjectsBanner from "../components/Projects/ProjectsBanner";
+import ProjectsAlert from "../components/Projects/ProjectsAlert";
 import Projects from '../constants/projects.json';
 
 export default {
@@ -16,6 +18,7 @@ export default {
     }),
     components: {
         ProjectsBanner,
+        ProjectsAlert,
     },
     metaInfo: {
         title: 'Projects'
