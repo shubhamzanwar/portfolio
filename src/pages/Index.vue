2564d389a36e2b087448e1ec2e8fff187a34e55b
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-04-25 14:53:46 +0530

    adding ht ehome banner in index route

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,20 @@
 <template>
   <Layout>
     <div class="home-page">
-      <div class="name-block">
-      </div>
-      <div class="name">Shubham <br> Zanwar</div>
+      <home-banner></home-banner>
     </div>
   </Layout>
 </template>
 
 <script>
+import HomeBanner from "../components/HomeBanner";
+
 export default {
   metaInfo: {
     title: 'Hello'
+  },
+  components: {
+    HomeBanner: HomeBanner
   }
 }
 </script>
@@ -19,37 +22,6 @@ export default {
 <style scoped>
   .home-page {
     flex: 1;
-    position: relative;
   }
 
-  .name-block {
-    position: absolute;
-    bottom: 8rem;
-    left: 6rem;
-    background: #B0F3D7;
-    height: 50vh;
-    width: 40vw;
-  }
-
-  .name {
-    position: absolute;
-    bottom: 8rem;
-    left: 7rem;
-    font-size: 5.6rem;
-    font-weight: bold;
-  }
-
-  @media only screen and (max-width: 768px) {
-    .name-block {
-      bottom: 3rem;
-      left: 0;
-      height: 24rem;
-      width: 100%;
-    }
-    .name {
-      bottom: 3rem;
-      left: 1rem;
-      font-size: 4rem;
-    }
-  }
 </style>
