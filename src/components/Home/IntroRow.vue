6f05af5ea79894bbeda8ce2f4df01a23caa4284d
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-04-25 15:02:50 +0530

    adding the intro row

---
+++ src/components/Home/IntroRow.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="intro-row">
+      <div class="content">
+        I’m a software developer from Bangalore, India. I’ve been designing and developing solutions since 2018
+      </div>
+      <div class="content margined-content">
+        I have been working in agile teams at Mckinsey digital labs and serving varied clients across different geographies
+      </div>
+      <div class="content">
+        I also actively contribute to open source projects and maintain a blog to document my learnings
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .intro-row {
+        display: flex;
+        margin-top: 3rem;
+    }
+
+    .content {
+        flex: 1;
+        font-size: 1.3rem;
+        color: #222222;
+        font-weight: 400;
+    }
+    .margined-content {
+        margin: 0 2rem;
+    }
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,19 +2,22 @@
   <Layout>
     <div class="home-page">
       <home-banner></home-banner>
+      <intro-row></intro-row>
     </div>
   </Layout>
 </template>
 
 <script>
 import HomeBanner from "../components/Home/HomeBanner";
+import IntroRow from "../components/Home/IntroRow";
 
 export default {
   metaInfo: {
     title: 'Hello'
   },
   components: {
-    HomeBanner: HomeBanner
+    HomeBanner,
+    IntroRow
   }
 }
 </script>
