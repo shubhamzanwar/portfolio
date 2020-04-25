70e9a1ffe1a185aadcb66f89a94c28450f3dc021
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-04-25 20:28:41 +0530

    adding blog page

---
+++ src/pages/Blog.vue
@@ -0,0 +1,39 @@
+<template>
+    <Layout>
+        <div class="blog-page">
+            <projects-banner />
+            <blog-alert />
+            <projects-grid />
+            <divider />
+        </div>
+    </Layout>
+</template>
+
+<script>
+import ProjectsBanner from "../components/Projects/ProjectsBanner";
+import BlogAlert from "../components/Blog/BlogAlert";
+import ProjectsGrid from "../components/Projects/ProjectsGrid";
+import Divider from "../components/Divider";
+import Projects from '../constants/projects.json';
+
+export default {
+    data: () => ({
+        projects: Projects
+    }),
+    components: {
+        ProjectsBanner,
+        BlogAlert,
+        ProjectsGrid,
+        Divider,
+    },
+    metaInfo: {
+        title: 'Blog'
+    }
+}
+</script>
+
+<style scoped>
+    .blog-page {
+        flex: 1;
+    }
+</style>
