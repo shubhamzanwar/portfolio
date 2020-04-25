c0b8aeb0d34ea8661b1e358568cb95e2f3ab535b
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-04-25 18:34:12 +0530

    removing unused icons

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { config, library } from '@fortawesome/fontawesome-svg-core'
-import { faCode, faTools, faBriefcase, faStar, faCircle, faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons'
+import { faStar, faCircle, faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 
 config.autoAddCss = false;
-library.add(faCode, faTools, faBriefcase, faStar, faCircle, faLongArrowAltRight);
+library.add(faStar, faCircle, faLongArrowAltRight);
 
 import DefaultLayout from '~/layouts/Default.vue'
 
