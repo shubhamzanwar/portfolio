4120fb8937a345dc5550a5f97c0b675eb00d0ad5
Shubham Zanwar <zanwar.shubham@gmail.com>
2020-04-25 15:20:28 +0530

    adding the alerts component

---
+++ src/components/Alert.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="base" :class="type">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        type: String
+    }
+}
+</script>
+
+<style scoped>
+    .base {
+        padding: 1rem;
+        font-size: 1.2rem;
+        font-weight: 400;
+        text-align: center;
+        border-radius: 10px;
+    }
+    .info {
+        background: #CCE5FF;
+        border: 1px solid #B8DAFF;
+        color: #004085;
+    }
+</style>
